be1c5498a24c9b8c7dba11914e568db03ba5a6ae
Eliot Berriot <contact@eliotberriot.com>
2014-10-02 14:10:41 +0200

    Initial commit

---
+++ app/tests/base.py
@@ -0,0 +1,5 @@
+import unittest
+
+class BaseTestCase(unittest.TestCase):
+    """A base Testcase other kii apps test cases inherit from"""
+    pass
---
+++ stream/tests/test_stream_item.py
@@ -0,0 +1,7 @@
+from ... import app
+
+
+class TestStreamItem(app.tests.base.BaseTestCase):
+
+    def test_stream_item_requires_a_stream(self):
+        pass        
