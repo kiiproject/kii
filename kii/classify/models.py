3b98eb7b9d53f28a7205704805015757d20f8b34
Eliot Berriot <contact@eliotberriot.com>
2014-10-07 19:06:21 +0200

    Removed stream field on Tag

--- kii/classify/models.py
+++ kii/classify/models.py
@@ -1,43 +1,23 @@
 from __future__ import unicode_literals
 from kii import base_models, stream as stream_app, permission
 from django.db import models
-from django.db.models.signals import pre_save
 from django.utils.translation import ugettext_lazy as _
-import django.core.exceptions
 from mptt.models import MPTTModel, TreeForeignKey
 
 class Tag(
     MPTTModel,
+    base_models.models.OwnerMixin,
     base_models.models.NameMixin):
     """
     A model for storing StreamItem instances"""
 
-    stream = models.ForeignKey(stream_app.models.Stream, related_name="tags")
-    items = models.ManyToManyField(stream_app.models.StreamItem, through='TagItem')
+    streamitems = models.ManyToManyField(stream_app.models.StreamItem, through='TagStreamItem', related_name="tags")
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
 
     class MPTTMeta:
         order_insertion_by = ['name']
         
-class TagItem(models.Model):
+class TagStreamItem(models.Model):
     """Many to many relationship between StreamItem and Tag"""
-    item = models.ForeignKey(stream_app.models.StreamItem)
+    streamitem = models.ForeignKey(stream_app.models.StreamItem)
     tag = models.ForeignKey(Tag)
-
-
-
-def tag_storableitem_created(sender, instance, **kwargs):
-    
-    if instance.pk is None:
-        # the instance is going to be created
-        if instance.tag.stream is not instance.item.stream:
-            raise django.core.exceptions.ValidationError(
-                "You cannot store a StreamItem in a Tag of a different Stream")
-
-pre_save.connect(tag_storableitem_created, sender=TagItem)
-
-
-def set_tag_owner(sender, instance, **kwargs):
-    instance.owner = instance.stream.owner
-
-pre_save.connect(set_tag_owner, sender=Tag)
--- kii/classify/tests/test_workspace.py
+++ kii/classify/tests/test_workspace.py
@@ -6,47 +6,28 @@ from django_dynamic_fixture import G
 
 class Tag(stream.tests.base.StreamTestCase):
     
-    def test_tag_requires_stream(self):
-        w = classify.models.Tag(name="yolo")
-
-        with self.assertRaises(AttributeError):
-            w.save()
 
     def test_can_add_stream_item_to_tag(self):
         s = self.streams[0]
-        w = classify.models.Tag(name="yolo", stream=s)
-        w.save()
+        t = G(classify.models.Tag, name="yolo", owner=s.owner)
 
-        si = stream.models.StreamItem(name="Hop la", stream=s)
-        si.save()
-        wsi = classify.models.TagItem(
-                tag=w, item=si)
-        wsi.save()
+        si = G(stream.models.StreamItem, name="Hop la", stream=s)
+        wsi = G(classify.models.TagStreamItem, tag=t, streamitem=si)
 
-    def test_tag_inherit_owner_from_stream(self):
-        w = G(classify.models.Tag, stream=self.streams[0])
-        self.assertEqual(w.owner, w.stream.owner)
+        self.assertEqual(classify.models.TagStreamItem.objects.all()[0], wsi)
 
-    def test_cannot_save_streamitem_in_tag_from_other_stream(self):
-        s0 = self.streams[0]
-        s1 = self.streams[1]
-        w0 = classify.models.Tag(name="s0", stream=s0)
-        w1 = classify.models.Tag(name="s1", stream=s1)
-        w0.save()
-        w1.save()
+    def test_can_query_tags_from_stream_item(self):
 
-        si = stream.models.StreamItem(name="Hop la", stream=s0)
-        si.save()
-        with self.assertRaises(django.core.exceptions.ValidationError):
-            wsi = classify.models.TagItem(
-                tag=w1, item=si)
-            wsi.save()
+        s = self.streams[0]
+        t = G(classify.models.Tag, name="yolo", owner=s.owner)
+
+        si = G(stream.models.StreamItem, name="Hop la", stream=s)
+        wsi = G(classify.models.TagStreamItem, tag=t, streamitem=si)
+
+        self.assertEqual(si.tags.all()[0], t)
 
     def test_can_store_tag_hierarchically(self):
         s = self.streams[0]
-        w0 = classify.models.Tag(name="level0", stream=s)
-        w0.save()
-        w1 = classify.models.Tag(name="level1", stream=s, parent=w0)
-        w1.save()
-        w2 = classify.models.Tag(name="level2", stream=s, parent=w1)
-        w2.save()
+        t0 = G(classify.models.Tag, name="level0")
+        t1 = G(classify.models.Tag, name="level1", parent=t0)
+        t2 = G(classify.models.Tag, name="level2", parent=t1)
