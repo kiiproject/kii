8ff7179c1ba86567a3a3bfb3911cec56b26a8ea7
Eliot Berriot <contact@eliotberriot.com>
2014-10-13 14:24:10 +0200

    Added constraint to force username and email in case of anonymous comment

--- kii/discussion/models.py
+++ kii/discussion/models.py
@@ -1,16 +1,29 @@
 from __future__ import unicode_literals
 from kii import base_models
-from django.db import models
+from django.db import models, IntegrityError
 from django.utils.translation import ugettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
-
+from django.conf import settings
 
 
 
 class CommentMixin(
     base_models.models.ContentMixin):
 
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="%(class)ss", editable=False, null=True, default=None)
+    username = models.CharField(max_length=50, default=None, null=True)
+    email = models.EmailField(default=False, null=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, **kwargs):
+
+        if self.user is None and (self.username is None or self.email is None):
+            raise IntegrityError(_('Username and email are required for anonymous comments'))
+
+        return super(CommentMixin, self).save(**kwargs)
+
 
 
 class DiscussionMixin(base_models.models.BaseMixin):
--- kii/discussion/tests/test_comment.py
+++ kii/discussion/tests/test_comment.py
@@ -1,16 +1,38 @@
 from ...user.tests import base
 from kii.tests.test_discussion import models
+from django.db import IntegrityError
 
 class CommentTestCase(base.UserTestCase):
+
+    def test_comment_can_belong_to_user(self):
+        c = self.G(models.DiscussionModelComment, user=self.users[0])
+
+        self.assertEqual(c.user, self.users[0])
 
     def test_can_comment_discussion_model(self):
         
         m = self.G(models.DiscussionModel)
-        c = self.G(models.DiscussionModelComment, subject=m)
+        c = self.G(models.DiscussionModelComment, subject=m, user=self.users[0])
 
         self.assertEqual(m.comments.all().first(), c)
 
     def test_comment_can_have_markdown_content(self):
 
-        c = self.G(models.DiscussionModelComment, content="# nice")
+        c = self.G(models.DiscussionModelComment, content="# nice", user=self.users[0])
         self.assertEqual(c.content.rendered, "<h1>nice</h1>")
+
+
+    def test_comment_anonymous_comment_requires_username_and_email(self):
+
+        m = self.G(models.DiscussionModel)
+        c = models.DiscussionModelComment(subject=m)
+
+        with self.assertRaises(IntegrityError):
+            c.save()
+
+        c.username = "something"
+        c.email = "hello@me.com"
+
+        c.save()
+
+
