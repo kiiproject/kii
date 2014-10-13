0d3f1aea24e9aade6449b3f4d1a4e5cd7842765e
Eliot Berriot <contact@eliotberriot.com>
2014-10-13 14:06:45 +0200

    Added very basic logic for comments

--- kii/discussion/tests/test_comment.py
+++ kii/discussion/tests/test_comment.py
@@ -9,3 +9,8 @@ class CommentTestCase(base.UserTestCase):
         c = self.G(models.DiscussionModelComment, subject=m)
 
         self.assertEqual(m.comments.all().first(), c)
+
+    def test_comment_can_have_markdown_content(self):
+
+        c = self.G(models.DiscussionModelComment, content="# nice")
+        self.assertEqual(c.content_rendered, "<h1>nice<h1>")
