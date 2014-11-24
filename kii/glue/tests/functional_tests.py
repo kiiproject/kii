ac8b5be5f95a840b5297abfc4313312dcd7b85f7
Eliot Berriot <contact@eliotberriot.com>
2014-11-24 17:29:25 +0100

    User home now lists enabled apps

--- kii/glue/tests/functional_tests.py
+++ kii/glue/tests/functional_tests.py
@@ -50,13 +50,16 @@ class GlueTest(StaticLiveServerTestCase):
         result_popup = self.browser.find_element_by_css_selector(".messages .success")
         self.assertIn('user.login.success', result_popup.text)
 
-        # his homepage lists currently enabled apps
+        # his homepage lists his tags and display his main stream
+        self.assertEqual(self.browser.current_url, self.url(reverse("kii:{0}:index".format(expected_apps[0].label))))
+
+        # on the top of the page, there is a menu which list enabled apps
+        # he clicks on the first app and is redirected to the app index
         apps = self.browser.find_elements_by_css_selector(".apps > li > a")
         expected_apps = app_manager.filter(user_access=True)
         self.assertEqual(len(apps), len(expected_apps))
         for i, app in enumerate(apps):
             self.assertEqual(app.text, expected_apps[i].verbose_name)
-
-        # he clicks on the first app and is redirected to the app index
         apps[0].click()
+        
         self.assertEqual(self.browser.current_url, self.url(reverse("kii:{0}:index".format(expected_apps[0].label))))
