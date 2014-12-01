a90e8f126809aeb9a14bd50393f1711e234cbc69
Eliot Berriot <contact@eliotberriot.com>
2014-12-01 14:29:09 +0100

    Added base navigation

--- kii/user/urls.py
+++ kii/user/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = patterns('',
 
     url(r'^login$', views.login, name="login"),
+    url(r'^logout$', views.logout, name="logout"),
     
 ) 
--- kii/user/views.py
+++ kii/user/views.py
@@ -7,3 +7,6 @@ def login(request, **kwargs):
     if request.user.is_authenticated():
         messages.success(request, "user.login.success")
     return r
+
+def logout(request, **kwargs):
+    return views.logout(request, **kwargs)
