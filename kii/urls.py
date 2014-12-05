e271e38c885415a3954dbf4b056029adc2424356
Eliot Berriot <contact@eliotberriot.com>
2014-12-05 13:56:43 +0100

    Added urlconf for easier inclusion

---
+++ kii/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url, include
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
+
+from kii.app import core
+
+kii_urls = core.apps.get_apps_urls()
+
+urlpatterns = patterns('', 
+    url(r'^(?P<username>\w+)/', include(kii_urls, namespace="user_area")),
+    url(r'^', include(kii_urls)),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy("kii:stream:index"), permanent=False)),
+)
