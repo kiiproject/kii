b5d00de66ca4c2cdba72a248ade28a9f067fca6a
Eliot Berriot <contact@eliotberriot.com>
2014-12-08 11:29:22 +0100

    Added StreamContextMixin that pass current stream to templates

---
+++ kii/stream/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.core.urlresolvers import reverse 
+
+from . import base
+from .. import models
+
+
+class TestStreamViews(base.StreamTestCase):
+    
+    def test_base_stream_view_passes_current_stream_to_context(self):
+
+        url = reverse('kii:stream:index')
+        stream = models.Stream.objects.get(owner=self.users[0], title=self.users[0].username)
+        self.login(self.users[0].username)
+        response = self.client.get(url)
+        
+        self.assertEqual(response.context['stream'], stream)
--- kii/stream/views.py
+++ kii/stream/views.py
@@ -4,23 +4,37 @@ from django.http import Http404
 from . import models
 from kii.base_models import views
 
-class Index(views.RequireAuthenticationMixin, views.OwnerMixinDetail):
+class StreamContextMixin(object):
+    """pass current requested stream into context"""
+
+    current_stream = None
+    def get_current_stream(self):
+        try:
+            self.current_stream = models.Stream.objects.get(owner=self.request.user.pk, title=self.request.user.username)
+            return self.current_stream
+        except models.Stream.DoesNotExist:
+            raise Http404
+
+    def get_context_data(self, **kwargs):
+        context = super(StreamContextMixin, self).get_context_data(**kwargs)
+        context['stream'] = self.current_stream
+
+        return context
+
+class Index(StreamContextMixin, views.RequireAuthenticationMixin, views.OwnerMixinDetail):
 
     template_name = "stream/stream/detail.html"
     streamitem_class = None
     def get_object(self):
 
-        try:
-            return models.Stream.objects.get(owner=self.request.user.pk, title=self.request.user.username)
-        except models.Stream.DoesNotExist:
-            raise Http404
+        return self.get_current_stream()
 
     def get_context_data(self, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
-        items = self.object.children.readable_by(self.request.user)
+        items = self.current_stream.children.readable_by(self.request.user)
         if self.streamitem_class is not None:
             # filter items using given class
-            items = items.select_subclasses(self.streamitem_class)
+            items = items.instance_of(self.streamitem_class)
         context["items"] = items
         return context
         
