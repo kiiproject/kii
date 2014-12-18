5bad3de60f68124f7bdab412acb39d5a5193cdb5
Eliot Berriot <contact@eliotberriot.com>
2014-12-18 22:50:27 +0100

    Fixed issue with angular and django template instructions delimiters

--- kii/stream/static/default/stream/js/moderation.js
+++ kii/stream/static/default/stream/js/moderation.js
@@ -1,7 +1,17 @@
-// Déclaration de l'app
-var app = angular.module('moderation', [])
 
-// Lancement du code quand angular est prêt
-app.run(function(){
-    alert('Hello !');
+/* La je recréé l'app à chaque fois pour des raisons de praticité, mais
+    dans un vrai code vous auriez ça dans un fichier à part, une seule
+    fois */
+
+// change aungular scope delimiter, or it will collide with django templates
+// thanks http://www.daveoncode.com/2013/10/17/how-to-make-angularjs-and-django-play-nice-together/
+var app = angular.module('tuto', [], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+// Création d'un controller, et attachement d'une variable au scope.
+app.controller('HelloCtrl', function($scope){
+    // Attachement d'une donnée au scope
+    $scope.hello = "Hello"
 })
