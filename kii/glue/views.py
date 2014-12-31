9554574c6bf1e89dbbb5bfbd10dc5c72026f0710
Eliot Berriot <contact@eliotberriot.com>
2014-12-31 17:32:51 +0100

    Homepage now list last published items

--- kii/glue/views.py
+++ kii/glue/views.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from kii.app.views import AppMixin
 from kii.user import get_kii_users_group
+from kii.stream.models import StreamItem
 
 
 class Home(AppMixin, TemplateView):
@@ -14,4 +15,6 @@ class Home(AppMixin, TemplateView):
 
         kii_users = get_kii_users_group().user_set.order_by('username')
         context['kii_users'] = kii_users
+        context['last_items'] = StreamItem.objects.readable_by(self.request.user)
+
         return context
