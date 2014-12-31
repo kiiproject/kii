0052fa4e5e2d8455abb7dedeef683d5ae65821da
Eliot Berriot <contact@eliotberriot.com>
2014-12-31 16:40:55 +0100

    Added syntax highlighting to default markdown

--- kii/glue/common_settings.py
+++ kii/glue/common_settings.py
@@ -86,9 +86,19 @@ LANGUAGES = (
     ('en', 'English'),
 )
 
+from django.utils.functional import curry
 import markdown
+from markdown.extensions.codehilite import makeExtension as CodeHilite # noqa
+
+md_filter = curry(markdown.markdown, extensions=[CodeHilite(css_class='code',
+                                                            linenums=False, 
+                                                            noclasses=True)])
+MARKDOWN_FUNCTION = md_filter
+
 #markupfield
 MARKUP_FIELD_TYPES = (
-    ('markdown', markdown.markdown),
+    ('markdown', md_filter),
     ('none', lambda s: s),
 )
+
+
