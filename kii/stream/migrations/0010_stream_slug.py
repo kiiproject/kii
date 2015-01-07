c1e08bbe498ee817684b3336bc4c5d9e7aa81924
Eliot Berriot <contact@eliotberriot.com>
2015-01-07 16:09:16 +0100

    Fixed migration errors

--- kii/stream/migrations/0010_stream_slug.py
+++ kii/stream/migrations/0010_stream_slug.py
@@ -26,5 +26,5 @@ class Migration(migrations.Migration):
             field=kii.base_models.fields.SlugField(populate_from=('title',), null=True, editable=False, blank=True),
             preserve_default=True,
         ),
-        #migrations.RunPython(set_stream_slug),
+        migrations.RunPython(set_stream_slug),
     ]
