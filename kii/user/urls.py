7f681cf0ad52464cfaa8f45d9ad1a20adbb84ac6
Eliot Berriot <contact@eliotberriot.com>
2015-01-14 16:31:15 +0100

    Fixed logout redirection

--- kii/user/urls.py
+++ kii/user/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^login$', views.login, name="login"), # NOQA
-    url(r'^logout$', views.logout, name="logout"),
+    url(r'^logout$', views.logout, {'next_page': '/'}, name="logout"),
 )
