fe25886c07d8255875638c928973361d3264b764
Eliot Berriot <contact@eliotberriot.com>
2015-01-17 23:24:32 +0100

    Inf act, markdown use parenthesis for link anchors...

--- kii/markdown/inlinepatterns.py
+++ kii/markdown/inlinepatterns.py
@@ -3,6 +3,8 @@ from markdown import inlinepatterns, util, Extension
 from django.core.urlresolvers import reverse
 
 USERNAME_RE = r'(@[\w@+-]+)'
+STREAM_RE = r'#(stream)\/([\w@+-_]+)'
+ITEM_RE = r'#(item)\/([\w@+-_]+)'
 
 
 class UsernamePattern(inlinepatterns.Pattern):
@@ -25,9 +27,67 @@ class UsernamePattern(inlinepatterns.Pattern):
         return el
 
 
+class HashPattern(inlinepatterns.Pattern):
+
+    def get_model(self):
+        raise NotImplementedError
+
+    def get_object(self):
+        raise NotImplementedError
+
+    def get_identifier(self, string):
+        return string
+
+    def handleMatch(self, m):
+
+        pattern = "#{0}/{1}".format(self.contenttype, m.group(3))
+
+        try:
+            identifier = self.get_identifier(m.group(3))
+        except ValueError:
+            return pattern
+        try:
+            target = self.get_object(identifier)
+        except self.get_model().DoesNotExist:
+            return pattern
+
+        el = util.etree.Element("a")
+        el.set('href', target.get_absolute_url())
+        el.text = pattern
+        return el
+
+
+class StreamHashPattern(HashPattern):
+
+    contenttype = "stream"
+
+    def get_model(self):
+        from kii.stream import models
+        return models.Stream
+
+    def get_object(self, identifier):
+        return self.get_model().objects.get(slug=identifier)
+
+
+class ItemHashPattern(HashPattern):
+    contenttype = "item"
+
+    def get_identifier(self, string):
+        return int(string)
+
+    def get_model(self):
+        from kii.stream import models
+        return models.StreamItem
+
+    def get_object(self, identifier):
+        return self.get_model().objects.get(pk=identifier)
+
+
 class KiiFlavoredMarkdownExtension(Extension):
     def extendMarkdown(self, md, md_globals):
         md.inlinePatterns["username"] = UsernamePattern(USERNAME_RE)
+        md.inlinePatterns["kii_stream"] = StreamHashPattern(STREAM_RE)
+        md.inlinePatterns["kii_stream_item"] = ItemHashPattern(ITEM_RE)
 
 
 def makeExtension(*args, **kwargs):
--- kii/markdown/tests/test_markdown.py
+++ kii/markdown/tests/test_markdown.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from kii.stream.tests.base import StreamTestCase
-from .. import inlinepatterns
+from kii.stream import models
+
+
 
 markdown = settings.MARKDOWN_FUNCTION
 
@@ -22,4 +24,42 @@ class TestMarkdown(StreamTestCase):
 
         self.assertEqual(rendered, expected)
 
-        
+    def test_markdown_can_reference_stream(self):
+        """test the #stream/slug syntax"""
+        stream = self.streams[0]
+        slug = stream.slug
+
+        content = "Test #stream/{0}".format(slug)
+        rendered = markdown(content)
+        expected = """<p>Test <a href="{0}">{1}</a></p>""".format(
+                    reverse("kii:stream:stream:index",
+                            kwargs={"stream": slug}),
+                    "#stream/{0}".format(slug))
+
+        self.assertEqual(rendered, expected)
+
+    def test_markdown_can_reference_item(self):
+        """test the #item/pk syntax"""
+        stream = self.streams[0]
+        i = models.StreamItem(title="Hello", root=stream)  
+        i.save()
+        content = "Test #item/{0}".format(i.pk)
+        rendered = markdown(content)
+        expected = """<p>Test <a href="{0}">{1}</a></p>""".format(
+                    reverse("kii:stream:stream:streamitem:detail",
+                            kwargs={"pk": i.pk, "stream": i.root.slug}),
+                    "#item/{0}".format(i.pk))
+
+        self.assertEqual(rendered, expected)
+
+    def test_hash_pattern_accepts_anchor(self):
+        stream = self.streams[0]
+        slug = stream.slug
+
+        content = "Test #stream/{0}(yolo)".format(slug)
+        rendered = markdown(content)
+        expected = """<p>Test <a href="{0}">yolo</a></p>""".format(
+                    reverse("kii:stream:stream:index",
+                            kwargs={"stream": slug}))
+
+        self.assertEqual(rendered, expected)
