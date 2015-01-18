929779bfabdff9bfa3d52832bfc56e9b2c85315d
Eliot Berriot <contact@eliotberriot.com>
2015-01-18 15:21:44 +0100

    Improved styling of autocomplete dropdown

--- kii/search/views.py
+++ kii/search/views.py
@@ -1,12 +1,13 @@
 from django import shortcuts
 from django.db.models import Q
 
-from kii.stream.models import Stream, StreamItem
+from kii.stream.models import Stream, StreamItem, ItemComment
 
 
 def NavigationAutocomplete(request,
     template_name='search/autocomplete/navigation.html'):
 
+    max_items = 5
     q = request.GET.get('q', '')
 
     queries = {}
@@ -14,11 +15,11 @@ def NavigationAutocomplete(request,
     queries['streams'] = Stream.objects.filter(
         Q(title__icontains=q) |
         Q(content__icontains=q)
-    ).readable_by(request.user).distinct()[:6]
+    ).readable_by(request.user).distinct()[:max_items]
 
     queries['items'] = StreamItem.objects.filter(
         Q(title__icontains=q) |
         Q(content__icontains=q)
-    ).readable_by(request.user).distinct()[:6]
+    ).readable_by(request.user).distinct()[:max_items]
 
     return shortcuts.render(request, template_name, queries)
